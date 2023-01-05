9a04037eb51cc79dd64dfc7851a7e226492c778b
zodang <1141ekdms@naver.com>
2023-01-05 16:33:53 +0900

    Fix: ComeUp 스크립트 수정
    해당 오브젝트가 배치되어 있는 땅 오브젝트의 높이와 오브젝트 y 크기에 맞춰 오브젝트가 상승

--- Assets/Scripts/BA_CompUp.cs
+++ Assets/Scripts/BA_CompUp.cs
@@ -4,24 +4,33 @@ using UnityEngine;
 
 public class BA_CompUp : MonoBehaviour
 {
+    //오브젝트 올라오는 속도
     public float speed = 2.0f;
-    private float height;
+    //오브젝트의 지면
+    public GameObject Ground;
+
+    private float ground;
+    private float targetValue_y;
+
     // Start is called before the first frame update
     void Start()
     {
-        height = transform.localScale.y / 2;
-        
+        //지면
+        ground = Ground.GetComponent<Transform>().position.y + Ground.GetComponent<Transform>().localScale.y / 2;
+
+        //오브젝트가 올라와야하는 y목표위치
+        targetValue_y = ground + transform.localScale.y / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        ComeUp(height);
+        ComeUp(targetValue_y);
     }
 
     void ComeUp(float Height)
     {
-        if (transform.position.y < Height)
+        if (transform.position.y < targetValue_y)
         {
             transform.Translate(new Vector3(0, speed, 0) * Time.deltaTime);
         }
