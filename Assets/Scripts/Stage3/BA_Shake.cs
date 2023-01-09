fabf12d135f95022445e2db0eed62c6b3b5f2614
zodang <1141ekdms@naver.com>
2023-01-10 01:42:11 +0900

    기능추가: 오브젝트 좌우로 흔들리는 기능(Shake.cs)
    피아노 줄 오브젝트에 적용

---
+++ Assets/Scripts/Stage3/BA_Shake.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BA_Shake : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public bool animationOn = false; // 숫자 2 누르면 애니메이션 작동
+
+    private Vector3 initialPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (animationOn == false)
+            {
+                animationOn = true;
+            }
+            else if (animationOn == true)
+            {
+                animationOn = false;
+            }
+        }
+
+        if (animationOn == true)
+        {
+            Vector3 currentPos = initialPos;
+            currentPos.x += 0.5f * Mathf.Sin(Time.time * speed);
+            transform.position = currentPos;
+        }
+    }
+}
