1dd7f80c869a486cc11f98d5b8547c261b4769e3
은광 정 <eoog333@gmail.com>
2023-01-18 23:08:01 +0900

    focusing

--- Assets/Scripts/NestedScrollManager.cs
+++ Assets/Scripts/NestedScrollManager.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class NestedScrollManager : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Scrollbar scrollbar;
+    public ScrollRect scrollRect;
     
     private const int SIZE = 5;
     private float[] pos = new float[SIZE];
@@ -32,22 +34,43 @@ public class NestedScrollManager : MonoBehaviour, IBeginDragHandler, IDragHandle
 
     public void OnDrag(PointerEventData eventData)
     {
+        
     }
 
+    public float to;
+    public float moveTime = 1f;
+    private float curTime;
     public void OnEndDrag(PointerEventData eventData)
     {
-
-        // 움직임 doTween DOmove
-        for (int i = 0; i < SIZE; i++)
+        
+        for(int i = 0; i < 5; i++)
+        if (scrollbar.value < pos[i] + distance * 0.5f && scrollbar.value > pos[i] - distance * 0.5f) //현재 value값과 가까운 맵으로 이동 
         {
-            if (scrollbar.value < pos[i] + distance * 0.5f && scrollbar.value > pos[i] - distance * 0.5f) //현재 value값과 가까운 맵으로 이동 
-            {
-                scrollbar.value = pos[i];
-            }
+            print(pos[i]);
+            StartCoroutine(CoMoveScrollRect(pos[i]));
+
+            break;
+            //scrollRect.DOVerticalNormalizedPos(to, 1, true);
+        }
+    }
+
+    IEnumerator CoMoveScrollRect(float to)
+    {
+        curTime = 0f;
+        float initValue = scrollbar.value;
+        
+        while (curTime < moveTime)
+        {
+            curTime += Time.deltaTime;
+
+            scrollbar.value = Mathf.Lerp(initValue, to, curTime / moveTime);
+
+            yield return null;
         }
     }
 
     void Update()
     {
+        print(scrollbar.value);
     }
 }
