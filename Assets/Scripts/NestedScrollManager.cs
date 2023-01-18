cb77b154acf7dde63a932ca3a725f05471a4ed55
은광 정 <eoog333@gmail.com>
2023-01-18 16:39:04 +0900

    updatemanager

--- Assets/Scripts/NestedScrollManager.cs
+++ Assets/Scripts/NestedScrollManager.cs
@@ -10,33 +10,19 @@ public class NestedScrollManager : MonoBehaviour, IBeginDragHandler, IDragHandle
     
     private const int SIZE = 5;
     private float[] pos = new float[SIZE];
-    private float distance;
+    private float distance, targetPos;
+    private bool isDrag;
+    
     void Start()
     {
-        // 거리에 따라 0~1인 pos 대입
-        // distance = 1f / (SIZE - 1);
-        /*
-        distance = 0.182243f;
-        pos[0] = 0.7313084f;
-        pos[1] = 0.5490654f;
-        pos[2] = 0.3668224f;
-        pos[3] = 0.1845794f;
-        pos[4] = 0.0023364f;
-        */
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        scrollbar.value = 1f; // 초기 화면 설정
+        distance = 0.25f; // 맵 중앙 피봇 사이 거리
+        // 각 맵이 중앙으로 포커싱 될때 scrollbar value 값
+        pos[0] = 0f;
+        pos[1] = 0.25f;
+        pos[2] = 0.5f;
+        pos[3] = 0.75f;
+        pos[4] = 1f;
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -46,16 +32,22 @@ public class NestedScrollManager : MonoBehaviour, IBeginDragHandler, IDragHandle
 
     public void OnDrag(PointerEventData eventData)
     {
-        
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
- 
+
+        // 움직임 doTween DOmove
+        for (int i = 0; i < SIZE; i++)
+        {
+            if (scrollbar.value < pos[i] + distance * 0.5f && scrollbar.value > pos[i] - distance * 0.5f) //현재 value값과 가까운 맵으로 이동 
+            {
+                scrollbar.value = pos[i];
+            }
+        }
     }
 
     void Update()
     {
-        
     }
 }
