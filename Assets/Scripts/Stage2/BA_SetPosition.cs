a5f2520e6b7a17c109c655e7dd091135e25d663f
zodang <1141ekdms@naver.com>
2023-01-31 17:57:41 +0900

    SetPosition() 랜덤 위치에서 제자리로 돌아오도록 수정

--- Assets/Scripts/Stage2/BA_SetPosition.cs
+++ Assets/Scripts/Stage2/BA_SetPosition.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class BA_SetPosition : BackgroundAnimation
 {
     public float speed = 10;
-    public Vector3 targetPos;   // 오브젝트가 이동할 목표 위치
+    public Vector3 targetPos;   // 원위치
+    public Vector3 randomPos;   // 랜덤위치
+    private float distance = 10.0f;
 
-    private bool animationOn = false; // 숫자 4 누르면 애니메이션 작동
+    private bool animationOn = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        targetPos = transform.position;
+        float randomX = Random.Range(targetPos.x - distance, targetPos.x + distance);
+        float randomZ = Random.Range(targetPos.z - distance, targetPos.z + distance);
 
+        //랜덤 위치로 오브젝트 재배치
+        randomPos = new Vector3(randomX, transform.position.y, randomZ);
+        transform.position = randomPos;   
     }
 
     // Update is called once per frame
@@ -20,8 +28,7 @@ public class BA_SetPosition : BackgroundAnimation
     {
         if (animationOn == true)
         {
-            Vector3 currentPos = transform.position;
-            transform.position = Vector3.MoveTowards(currentPos, targetPos, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
         }
     }
 
