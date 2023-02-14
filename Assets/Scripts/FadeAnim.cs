145a188a49d66c73f3521eaf5e2ee91b273538e0
은광 정 <eoog333@gmail.com>
2023-02-15 07:25:55 +0900

    fadeAnim

--- Assets/Scripts/FadeAnim.cs
+++ Assets/Scripts/FadeAnim.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,19 +12,20 @@ public class FadeAnim : MonoBehaviour
     private float[] pos = new float[SIZE];
     public Scrollbar scrollbar;
     private Color currentColor;
-    private float fadeInValue = 0.6f;
+    public float firstValue = 0.5f;
     
     
     void Start()
     {
-        _meshRenderer = GetComponent<MeshRenderer>();
+        _meshRenderer = transform.GetComponent<MeshRenderer>();
         currentColor = _meshRenderer.material.color;
-        if (this.GameObject() != GameObject.Find("Map1"))
+        
+        if (gameObject.transform.parent.parent.name != "stage1")
         {
-            currentColor.a = fadeInValue;
+            currentColor.a = firstValue; 
             _meshRenderer.material.color = currentColor;
         }
-
+        
         pos[0] = 0f;
         pos[1] = 0.5f;
         pos[2] = 1f;
@@ -31,14 +33,67 @@ public class FadeAnim : MonoBehaviour
     
     void Update()
     {
-        if (this.GameObject() == GameObject.Find("Map3"))
-        {
-            Color fadeInColor = _meshRenderer.material.color;
-            while (scrollbar.value < pos[3] && scrollbar.value >= pos[2])
+        StartCoroutine(FadeUp());
+        StartCoroutine(FadeDown());
+    }
+
+
+    IEnumerator FadeUp()
+    {
+        Color curColor = _meshRenderer.material.color;
+        
+        if(gameObject.transform.parent.parent.name == "stage2" || gameObject.transform.parent.parent.parent.name == "stage2")
+            while (scrollbar.value < 1f && scrollbar.value >= 0.5f)
             {
-                fadeInColor.a = fadeInValue + scrollbar.value;
-                _meshRenderer.material.color = fadeInColor;
+                curColor.a = firstValue + ((1/scrollbar.value)-1f); //0-1
+                if (curColor.a >= 1f)
+                    curColor.a = 1f;
+                
+                _meshRenderer.material.color = curColor;
+
+                yield return null;
             }
-        }
+        
+        if(gameObject.transform.parent.parent.name == "stage3" || gameObject.transform.parent.parent.parent.name == "stage3")
+            while (scrollbar.value < 0.5f && scrollbar.value >= 0f)
+            {
+                curColor.a = firstValue + ((1/(scrollbar.value+0.5f))-1f); 
+                if (curColor.a >= 1f)
+                    curColor.a = 1f;
+                
+                _meshRenderer.material.color = curColor;
+
+                yield return null;
+            }
+    }
+
+    IEnumerator FadeDown()
+    {
+        Color curColor = _meshRenderer.material.color;
+
+        if(gameObject.transform.parent.parent.name == "stage1" || gameObject.transform.parent.parent.parent.name == "stage1")
+            while (scrollbar.value < 1f && scrollbar.value >= 0.5f)
+            {
+                curColor.a = 1f - ((1/scrollbar.value)-1f);   //0-1
+                if (curColor.a <= 0.6f)
+                    curColor.a = 0.6f;
+                
+                _meshRenderer.material.color = curColor;
+
+                yield return null;
+            }
+            
+        
+        if(gameObject.transform.parent.parent.name == "stage2" || gameObject.transform.parent.parent.parent.name == "stage2")
+            while (scrollbar.value < 0.5f && scrollbar.value >= 0f)
+            {
+                curColor.a = 1f - ((1/(scrollbar.value+0.5f))-1f); //0.5-0
+                if (curColor.a <= 0.6f)
+                    curColor.a = 0.6f;
+                
+                _meshRenderer.material.color = curColor;
+
+                yield return null;
+            }
     }
 }
