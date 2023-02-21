2f5730dd278c288ef12a11622a80726be89e8711
Do Hyung Kim <wjp229@naver.com>
2023-02-21 17:07:11 +0900

    Update FadeAnimations

--- Assets/Scripts/FadeAnim.cs
+++ Assets/Scripts/FadeAnim.cs
@@ -7,36 +7,37 @@ using UnityEngine.UI;
 
 public class FadeAnim : MonoBehaviour
 {
-    private MeshRenderer[] _meshRenderer;
+    [SerializeField] private MeshRenderer[] _meshRenderer;
     private const int SIZE = 3;
     private float[] pos = new float[SIZE];
     public Scrollbar scrollbar;
-    private Color[] currentColor;
+    private List<Color> currentColor = new List<Color>();
     public float firstValue = 0.5f;
 
+    public GameObject parentTransfrom;
 
     void Start()
     {
-
         _meshRenderer = transform.GetComponentsInChildren<MeshRenderer>();
+        if (_meshRenderer.Length == 0)
+        {
+            Destroy(GetComponent<FadeAnim>());
+        }
 
         for (int i = 0; i < _meshRenderer.Length; i++)
         {
-            currentColor[i] = _meshRenderer[i].material.color;
+            currentColor.Add(_meshRenderer[i].material.color);
         }
 
 
-
         for (int i = 0; i < _meshRenderer.Length; i++)
-            if (gameObject.transform.parent.name != "stage1")
+            if (parentTransfrom.name != "stage1")
             {
-                currentColor[i].a = firstValue;
+                Color c = currentColor[i];
+                c.a = firstValue;
+                currentColor[i] = c;
                 _meshRenderer[i].material.color = currentColor[i];
             }
-
-        pos[0] = 0f;
-        pos[1] = 0.5f;
-        pos[2] = 1f;
     }
 
     void Update()
@@ -48,63 +49,82 @@ public class FadeAnim : MonoBehaviour
 
     IEnumerator FadeUp()
     {
-        for (int i = 0; i < _meshRenderer.Length; i++)
-            if (gameObject.transform.parent.name == "stage2" || gameObject.transform.parent.parent.name == "stage2")
+        {
+            if (parentTransfrom.name == "stage2")
+            {
                 while (scrollbar.value < 1f && scrollbar.value >= 0.5f)
-                {
-                    currentColor[i].a = firstValue + ((1 / scrollbar.value) - 1f); //0-1
-                    if (currentColor[i].a >= 1f)
-                        currentColor[i].a = 1f;
+                    for (int i = 0; i < _meshRenderer.Length; i++)
+                    {
+                        Color c = currentColor[i];
+                        c.a = firstValue + ((1 / scrollbar.value) - 1f); //0-1
+                        if (currentColor[i].a >= 1f)
+                            c.a = 1f;
 
+                        currentColor[i] = c;
+                        _meshRenderer[i].material.color = currentColor[i];
+
+                        yield return null;
+                    }
+            }
+        }
+
+        if (parentTransfrom.name == "stage3")
+        {
+            while (scrollbar.value < 0.5f && scrollbar.value >= 0f)
+                for (int i = 0; i < _meshRenderer.Length; i++)
+                {
+                    Color c = currentColor[i];
+                    c.a = firstValue + ((1 / (scrollbar.value + 0.5f)) - 1f);
+                    if (currentColor[i].a >= 1f)
+                        c.a = 1f;
+
+                    currentColor[i] = c;
                     _meshRenderer[i].material.color = currentColor[i];
 
                     yield return null;
                 }
+        }
+    }
 
-        for (int i = 0; i < _meshRenderer.Length; i++)
-            if (gameObject.transform.parent.name == "stage3" || gameObject.transform.parent.parent.name == "stage3")
+    public void CallFadeDown()
+    {
+        StartCoroutine(FadeDown());
+    }
+
+    IEnumerator FadeDown()
+    {
+        if (parentTransfrom.name == "stage1")
+            while (scrollbar.value < 1f && scrollbar.value >= 0.5f)
             {
-                while (scrollbar.value < 0.5f && scrollbar.value >= 0f)
+                for (int i = 0; i < _meshRenderer.Length; i++)
                 {
-                    currentColor[i].a = firstValue + ((1 / (scrollbar.value + 0.5f)) - 1f);
-                    if (currentColor[i].a >= 1f)
-                        currentColor[i].a = 1f;
+                    Color c = currentColor[i];
+                    c.a = 2f - (1 / scrollbar.value); //0-1
+                    if (c.a <= 0.6f)
+                        c.a = 0.6f;
 
+                    Debug.Log(c.a);
+                    
+                    currentColor[i] = c;
                     _meshRenderer[i].material.color = currentColor[i];
 
                     yield return null;
                 }
             }
-    }
 
-    IEnumerator FadeDown()
-    {
-        for (int i = 0; i < _meshRenderer.Length; i++)
-            if (gameObject.transform.parent.name == "stage1" || gameObject.transform.parent.parent.name == "stage1")
-                while (scrollbar.value < 1f && scrollbar.value >= 0.5f)
+        if (parentTransfrom.name == "stage2")
+            while (scrollbar.value < 0.5f && scrollbar.value >= 0f)
+                for (int i = 0; i < _meshRenderer.Length; i++)
                 {
-                    currentColor[i].a = 1f - ((1 / scrollbar.value) - 1f); //0-1
+                    Color c = currentColor[i];
+                    c.a = 1f - ((1 / (scrollbar.value + 0.5f)) - 1f); //0.5-0
                     if (currentColor[i].a <= 0.6f)
-                        currentColor[i].a = 0.6f;
+                        c.a = 0.6f;
 
-                    _meshRenderer[i].material.color = currentColor[i];
-
-                    yield return null;
-                }
-
-        for (int i = 0; i < _meshRenderer.Length; i++)
-            if (gameObject.transform.parent.name == "stage2" || gameObject.transform.parent.parent.name == "stage2")
-                while (scrollbar.value < 0.5f && scrollbar.value >= 0f)
-                {
-                    currentColor[i].a = 1f - ((1 / (scrollbar.value + 0.5f)) - 1f); //0.5-0
-                    if (currentColor[i].a <= 0.6f)
-                        currentColor[i].a = 0.6f;
-
+                    currentColor[i] = c;
                     _meshRenderer[i].material.color = currentColor[i];
 
                     yield return null;
                 }
     }
 }
-
-
