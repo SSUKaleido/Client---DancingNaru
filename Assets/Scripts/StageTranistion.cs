e0b938672ba65c51ea9708d9214820aa52d9fd31
Do Hyung Kim <wjp229@naver.com>
2023-03-03 10:41:22 +0900

    Update Transition Button

--- Assets/Scripts/StageTranistion.cs
+++ Assets/Scripts/StageTranistion.cs
@@ -1,13 +1,11 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class goToStage : MonoBehaviour
+public class StageTranistion : MonoBehaviour
 {
-    private Button button;
+    private Button button_Stage;
     public Scrollbar scrollbar;
     private float[] pos = new float[3];
 
@@ -15,28 +13,25 @@ public class goToStage : MonoBehaviour
     {
         if (gameObject.transform.parent.name == "Map1")
         {
-            Debug.Log("스테이지1 이동");
-            //SceneManager.LoadScene("stage1");
+           // SceneManager.LoadScene("stage1");
         }
 
         if (gameObject.transform.parent.name == "Map2")
         {
-            Debug.Log("스테이지2 이동");
             //SceneManager.LoadScene("stage2");
         }
 
         if (gameObject.transform.parent.name == "Map3")
         {
-            Debug.Log("스테이지3 이동");
-            //SceneManager.LoadScene("stage3");
+           //SceneManager.LoadScene("stage3");
         }
     }
 
     void Start()
     {
-        button = GetComponent<Button>();
-        button.onClick.AddListener(OnClickButton);
-        button.interactable = false;
+        button_Stage = GetComponent<Button>();
+        button_Stage.onClick.AddListener(OnClickButton);
+        button_Stage.interactable = false;
         
         pos[0] = 0f;
         pos[1] = 0.5f;
@@ -50,27 +45,27 @@ public class goToStage : MonoBehaviour
 
     IEnumerator ButtonOn()
     {
-        if (button.interactable)
-            button.interactable = false;
+        if (button_Stage.interactable)
+            button_Stage.interactable = false;
         
         if (gameObject.transform.parent.name == "Map1")
             while (scrollbar.value < 1.1f && scrollbar.value > 0.9f) 
             {
-                button.interactable = true;
+                button_Stage.interactable = true;
                 yield return null;
             }
-
+        
         if (gameObject.transform.parent.name == "Map2")
             while (scrollbar.value < 0.6f && scrollbar.value > 0.4f)
             {
-                button.interactable = true;
+                button_Stage.interactable = true;
                 yield return null;
             }
-
+        
         if (gameObject.transform.parent.name == "Map3")
             while (scrollbar.value < 0.1f && scrollbar.value > -0.1f)
             { 
-                button.interactable = true;
+                button_Stage.interactable = true;
                 yield return null;
             }
     }
