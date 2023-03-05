41200b2512c77ff481ddce66b689aed2d7f07156
zodang <1141ekdms@naver.com>
2023-03-05 23:32:26 +0900

    스테이지별 엔딩 애니메이션 추가

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StageManager : MonoBehaviour
 {
@@ -13,6 +14,10 @@ public class StageManager : MonoBehaviour
 
     public GameObject clearCube;
     public GameObject startCube;
+
+    public GameObject CameraPoint;
+    private CameraController CameraControllerScript;
+
 
     private void Awake()
     {
@@ -47,6 +52,24 @@ public class StageManager : MonoBehaviour
     public void GameOver()
     {
         controller.StopPlayer();
+
+        //Camera Enidng Animation
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "Stage_1_Scene":
+                CameraPoint.GetComponent<CameraController>().panValue = 135;
+                CameraPoint.GetComponent<CameraController>().state = CameraController.CameraState.Pan;
+                break;
+
+            case "Stage_2_Scene":
+                CameraPoint.GetComponent<CameraController>().zoomValue = 65;
+                CameraPoint.GetComponent<CameraController>().state = CameraController.CameraState.Zoom;
+                break;
+            case "Stage_3_Scene":
+                CameraPoint.GetComponent<CameraController>().boomValue = 5;
+                CameraPoint.GetComponent<CameraController>().state = CameraController.CameraState.Boom;
+                break;
+        }
         
         float clearDist = Vector3.Distance(clearCube.transform.position, startCube.transform.position);
         float userDist = Vector3.Distance(controller.transform.position, startCube.transform.position);
