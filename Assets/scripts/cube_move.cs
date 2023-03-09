d9d46a62951896d0622243a33b5808d147d7dd8e
정홍주 <forcloud300@gmail.com>
2023-03-09 10:02:31 +0900

    crown effect done

--- Assets/scripts/cube_move.cs
+++ Assets/scripts/cube_move.cs
@@ -5,8 +5,24 @@ using UnityEngine;
 public class cube_move : MonoBehaviour
 {
     public float speed = 5.0f;
-    public ParticleSystem particleObject;
+    public ParticleSystem particleObject; //변경할 material 넣을 곳
+    public Material[] mat = new Material[2];
+    public GameObject crownObj;
 
+    void Start()
+    {
+        crownObj = GameObject.Find("2d_CROWN");
+    }
+
+
+    public void ChangeCrownMat()
+    {
+        int i = 0;
+        i =  ++i % 2;
+
+        //change material
+        crownObj.GetComponent<MeshRenderer>().material = mat[i];
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,8 +31,11 @@ public class cube_move : MonoBehaviour
             Debug.Log("충돌");
             Destroy(other.gameObject); //충돌시 crown이 사라짐
 
-            particleObject.Play(); //파티클 시스템 실행
+            particleObject.Play(); //파티클 시스템 실행(빛무리가 나옴)
+            ChangeCrownMat();
         }
+
+        
     }
 
     // Update is called once per frame
