e10e5b3c10fee6b11aa897c75615f73542e2ae94
zodang <1141ekdms@naver.com>
2023-03-20 03:21:43 +0900

    스테이지2 배경 애니메이션 적용

--- Assets/Scripts/Stage2/BA_Rotate.cs
+++ Assets/Scripts/Stage2/BA_Rotate.cs
@@ -6,6 +6,7 @@ public class BA_Rotate : BackgroundAnimation
 {
     public float speed = 30.0f; //speed 값이 양수이면 오른쪽으로, 음수이면 왼쪽으로 이동
     private bool animationOn = false;
+    public Vector3 rotateAxis = Vector3.up;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +17,19 @@ public class BA_Rotate : BackgroundAnimation
     // Update is called once per frame
     void Update()
     {
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        Vector3[] positions = new Vector3[renderers.Length];
+        for (int i = 0; i < positions.Length; i++)
+        {
+            positions[i] = renderers[i].bounds.center;
+        }
+
         if (animationOn == true)
         {
-            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+            for (int i = 0; i < positions.Length; i++)
+            {
+                transform.RotateAround(positions[i], rotateAxis, speed * Time.deltaTime);
+            }
         }
     }
 
--- Assets/Scripts/Stage2/BA_RotateAround.cs
+++ Assets/Scripts/Stage2/BA_RotateAround.cs
@@ -6,10 +6,12 @@ public class BA_RotateAround : BackgroundAnimation
 {
     public float speed = 150.0f; //speed 값이 양수이면 위로, 음수이면 아래로 이동
     public bool animationOn = false; // 숫자3 누르면 애니메이션 작동
-    public Vector3 rotationPoint = new Vector3(100, 0, 0);  // 회전 기준점
+    //public Vector3 rotationPoint = new Vector3(100, 0, 0);  // 회전 기준점
 
     private int waitingTime = 1; // 1초마다 끊어서 회전
     private float timer = 0;
+
+    public Vector3 rotateAxis = Vector3.up;
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +22,32 @@ public class BA_RotateAround : BackgroundAnimation
     // Update is called once per frame
     void Update()
     {
+
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        Vector3[] positions = new Vector3[renderers.Length];
+        for (int i = 0; i < positions.Length; i++)
+        {
+            positions[i] = renderers[i].bounds.center;
+        }
+
+        // if (animationOn == true)
+        // {
+        //     for (int i = 0; i < positions.Length; i++)
+        //     {
+        //         transform.RotateAround(positions[i], rotateAxis, speed * Time.deltaTime);
+        //     }
+        // }
+
         if (animationOn == true)
         {
             timer = timer + Time.deltaTime;
 
             if (timer > waitingTime)
             {
-                transform.RotateAround(rotationPoint, Vector3.up, speed * Time.deltaTime);
+                for (int i = 0; i < positions.Length; i++)
+                {
+                    transform.RotateAround(positions[i], rotateAxis, speed * Time.deltaTime);
+                }
                 timer = 0;
             }
         }
